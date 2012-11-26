924dde8a0fae3ab095f276a27c857a08f1837396
Fabian Melters <melters@gmail.com>
2012-11-26 14:18:37 +0100

    optimized and adapted for actual bitly-api
    + removed changelog from script
    + url-string optimized

--- scripts/bitly.pl
+++ scripts/bitly.pl
@@ -14,13 +14,6 @@
 # you can see the link in your bit.ly account
 #
 #######################################################################
-# Changelog:
-# - added setting for posting the requested url in active channel
-#   or private
-# - added setting for adding a text which is set in front of the url
-#   if u are posting in the active channel
-#
-#######################################################################
 
 use strict;
 use WWW::Mechanize;
@@ -28,7 +21,7 @@ use WWW::Mechanize;
 use vars qw($VERSION %IRSSI);
 
 use Irssi qw(command_bind);
-$VERSION = '0.1';
+$VERSION = '1.01';
 %IRSSI = (
  authors        => 'xx4h',
  contact        => 'melters@gmail.com',
@@ -36,56 +29,59 @@ $VERSION = '0.1';
  description    => 'This script shortens'.
                    'a url via bit.ly api'.
                    'using a bit.ly account',
- changed        => 'Mon, Feb 22 23:27 2011',
+ changed        => 'Mon, Nov 26 2011',
  license        => 'Public Domain',
 );
 
 
-
 sub short {
 my ($data, $server, $witem) = @_;
+
   my $login = Irssi::settings_get_str("bitly_login");
-  my $apikey = Irssi::settings_get_str("bitly_apikey");
+  my $apiKey = Irssi::settings_get_str("bitly_apikey");
   my $output = Irssi::settings_get_str("bitly_output");
-if (not $data) {
-  Irssi::print('Usage: /bitly [url]', MSGLEVEL_CRAP);
-  return 0;
-}
+
+  if (!$login || !$apiKey) {
+      Irssi::print('You didn\'t set the login and/or apiKey', MSGLEVEL_CRAP);
+      Irssi::print('You can find both here: http://bitly.com/a/your_api_key',MSGLEVEL_CRAP);
+      return 0;
+  }
+
+  if (not $data) {
+    Irssi::print('Usage: /bitly [url]', MSGLEVEL_CRAP);
+    return 0;
+  }
 
   my $shortthis = $data;
 
   $shortthis =~ s/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
 
-  # split url into parts...not a very nice solution =)
-  my $url_base = "http://api.bitly.com/v3/shorten?login=";
-  my $url_apiKey = "&apiKey=";
-  my $url_longUrl = "&longUrl=";
-  my $url_format = "&format=txt";
+  # building the bitly-api-string
+  my $url_bitly_api = "https://api-ssl.bitly.com/v3/shorten?format=txt&login=$login&apiKey=$apiKey&longUrl=$shortthis";
 
-  # get url back to one
-  my $url = $url_base.$login.$url_apiKey.$apikey.$url_longUrl.$shortthis.$url_format;
-
+  # set up the www-agent
   my $agent = WWW::Mechanize->new();
 
   eval{
-      $agent->get($url);
+      $agent->get($url_bitly_api);
   };
   if ($@){
       Irssi::print("Bitly warning: something went wrong, check url and try again", MSGLEVEL_CRAP);
       return 0;
   }
-  my $shortened = $agent->content(format => 'text');
-if (Irssi::settings_get_bool("bitly_public") == 0) {
-  Irssi::print("Requested URL: $shortened", MSGLEVEL_CRAP);
-}
-if (Irssi::settings_get_bool("bitly_public") == 1) {
-  Irssi::print("$output, $shortened", MSGLEVEL_PUBLIC);
-}
+  my $shortened = $agent->content(format => 'txt');
+  chomp($shortened);
+  if (Irssi::settings_get_bool("bitly_public") == 0) {
+    Irssi::print("Shortened URL: $shortened", MSGLEVEL_CRAP);
+  }
+  if (Irssi::settings_get_bool("bitly_public") == 1) {
+    Irssi::print("$output, $shortened", MSGLEVEL_PUBLIC);
+  }
 
 
 }
-Irssi::settings_add_str("misc", "bitly_apikey", '');
-Irssi::settings_add_str("misc", "bitly_login", '');
+Irssi::settings_add_str("misc", "bitly_apikey", undef);
+Irssi::settings_add_str("misc", "bitly_login", undef);
 Irssi::settings_add_str("misc", "bitly_output", 'here teh url');
 Irssi::settings_add_bool("misc", "bitly_public", 0);
 
